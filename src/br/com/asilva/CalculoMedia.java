812438bf5c13d2d3011bae6a3dfce6686aca2406
Hanteek <alex.silva01@outlook.com.br>
2023-09-25 19:51:15 -0300

    Adicionando arquivos da calculadora de média com controle de fluxo

--- src/br/com/asilva/CalculoMedia.java
+++ src/br/com/asilva/CalculoMedia.java
@@ -6,16 +6,25 @@ public class CalculoMedia {
     private double terceiroNumero;
     private double quartoNumero;
 
-    public CalculoMedia(double primeiroNumero, double segundoNumero, double terceiroNumero, double quartoNumero){
+    public CalculoMedia(double primeiroNumero, double segundoNumero, double terceiroNumero, double quartoNumero) {
         this.primeiroNumero = primeiroNumero;
         this.segundoNumero = segundoNumero;
         this.terceiroNumero = terceiroNumero;
         this.quartoNumero = quartoNumero;
     }
 
-    public double calcularMedia(){
-        return (primeiroNumero + segundoNumero + terceiroNumero + quartoNumero)/ 4;
+    public double calcularMedia() {
+        return (primeiroNumero + segundoNumero + terceiroNumero + quartoNumero) / 4;
     }
 
-
+    public String obterMensagemResultado() {
+        double media = calcularMedia();
+        if (media >= 7) {
+            return "O aluno está aprovado!";
+        } else if (media >= 5) {
+            return "O aluno está em recuperação.";
+        } else {
+            return "O aluno está reprovado.";
+        }
+    }
 }
